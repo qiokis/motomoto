f1de8eed0fd4f372138238d1234e9defab455feb
Anton Pechkin <xecros1@ya.ru>
2020-09-16 19:24:49 +0800

    fix 2.4

--- lab2/2.4.py
+++ lab2/2.4.py
@@ -1,9 +1,11 @@
 import re
-patt = re.compile(r"\d\w+")
+patt = re.compile(r"as\d")
 file = open("C:\\Users\\Anton\\Desktop\\test.txt", "r")
 file1 = open("C:\\Users\\Anton\\Desktop\\test1.txt", "w")
 mas = file.readline().split()
-for i in mas:
-    if re.match(patt, i):
-        file1.write(i)
+while mas:
+    for i in mas:
+        if re.match(patt, i):
+            file1.write(i+" ")
+    mas = file.readline().split()
 file1.close()
