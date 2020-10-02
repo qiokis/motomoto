e3b3c556a5f8aabd6a33d0745c4cb2279d882f34
Anton Pechkin <xecros1@ya.ru>
2020-10-02 19:34:19 +0800

    fixed

--- lab2/2.1.py
+++ lab2/2.1.py
@@ -1,6 +1,8 @@
+import re
+
 mas = [i for i in input().split()]
 temp = mas[0]
 for i in mas[1:]:
-    if temp[-1] == i[0]:
+    if temp[-1] == i[0] and re.match(r'[A-Za-z]', temp[-1]):
         print(temp, i)
     temp = i
